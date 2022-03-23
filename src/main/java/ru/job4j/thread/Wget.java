9f0276b83808c15b3df6e0b84547d57e7bbd87a7
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-03-23 13:42:22 +0300

    4. Скачивание файла с ограничением. [#144271 #278921]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,7 +1,6 @@
 package ru.job4j.thread;
 
 import java.io.BufferedInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -10,33 +9,54 @@ import java.util.Calendar;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileName;
 
     public Wget(String url, int speed) {
         this.url = url;
         this.speed = speed;
+        String[] arr = url.split("/");
+        this.fileName = arr[arr.length - 1];
     }
 
     @Override
     public void run() {
-        long start = Calendar.getInstance().getTimeInMillis();
-        long needTime = 1024 / speed;
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long downloadTime = Calendar.getInstance().getTimeInMillis() - start;
-                if (downloadTime < needTime) {
-                    Thread.sleep(needTime - downloadTime);
+            long bytesWrited = 0;
+            try {
+                long start = Calendar.getInstance().getTimeInMillis();
+                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                    if (bytesWrited >= speed) {
+                        long deltaTime = Calendar.getInstance().getTimeInMillis() - start;
+                        if (deltaTime < 1000) {
+                            Thread.sleep(1000 - deltaTime);
+                        }
+                        bytesWrited = 0;
+                        start = Calendar.getInstance().getTimeInMillis();
+                    }
+                    fileOutputStream.write(dataBuffer, 0, bytesRead);
+                    bytesWrited += bytesRead;
                 }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
+
+        }
+    }
+
+    private static void validate(int argsNum) {
+        if (argsNum != 2) {
+            throw new IllegalArgumentException("There must be two arguments. "
+                    + "First argument must be url, second argument - download speed.");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        Wget.validate(args.length);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
