1578ab1b55fc1de8195fb70c8b8a15a981e245b2
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-04-01 14:02:44 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

--- src/test/java/ru/job4j/notify/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/notify/SimpleBlockingQueueTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 public class SimpleBlockingQueueTest {
 
     @Test
-    public void whenProducerConsumerIsCorrect() throws InterruptedException{
+    public void whenProducerConsumerIsCorrect() throws InterruptedException {
         List<Integer> rsl = new ArrayList<>();
         SimpleBlockingQueue<Integer> simpleBlockingQueue = new SimpleBlockingQueue<>(1);
         Thread producer = new Thread(() -> {
