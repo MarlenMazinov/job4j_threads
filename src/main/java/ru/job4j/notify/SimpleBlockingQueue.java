42a8716174e0ce4cfda3a8b1d58bb3423976d2c5
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-04-01 14:01:29 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/notify/SimpleBlockingQueue.java
@@ -0,0 +1,35 @@
+package ru.job4j.notify;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
+    private final int size;
+
+    public SimpleBlockingQueue(int size) {
+        this.size = size;
+    }
+
+    public synchronized void offer(T value) throws InterruptedException {
+        while (queue.size() == size) {
+            this.wait();
+        }
+        this.notify();
+        queue.offer(value);
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        while (queue.size() == 0) {
+            this.wait();
+        }
+        this.notify();
+        return queue.poll();
+    }
+}
---
+++ src/test/java/ru/job4j/notify/SimpleBlockingQueueTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.notify;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenProducerConsumerIsCorrect() throws InterruptedException{
+        List<Integer> rsl = new ArrayList<>();
+        SimpleBlockingQueue<Integer> simpleBlockingQueue = new SimpleBlockingQueue<>(1);
+        Thread producer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    simpleBlockingQueue.offer(i);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    rsl.add(simpleBlockingQueue.poll());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertEquals(rsl, List.of(0, 1, 2));
+    }
+}
